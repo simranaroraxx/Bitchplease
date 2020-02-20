ca2a67047cb6db478ca5c59e2a8eacf92d58510b
simranaroraxx <arora11@sheridancollege.ca>
2020-02-19 21:43:45 -0500

    First commit

---
+++ src/bitchplease/BitchPlease.java
@@ -0,0 +1,15 @@
+
+package bitchplease;
+
+
+public class BitchPlease {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hi tryna study");
+    }
+    
+}
